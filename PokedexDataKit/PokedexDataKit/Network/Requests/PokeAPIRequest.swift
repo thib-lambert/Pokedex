65e013c2de4af6ea0eb89bfaeffa7334b4fe9131
Thibaud LAMBERT <thib.lambert@icloud.com>
2024-07-14 18:15:07 +0200

    [PokedexDataKit][Network] Create PokeAPIRequest

---
+++ PokedexDataKit/PokedexDataKit/Network/Requests/PokeAPIRequest.swift
@@ -0,0 +1,27 @@
+//
+//  PokeAPIRequest.swift
+//  PokedexDataKit
+//
+//  Created by Thibaud Lambert on 14/07/2024.
+//
+
+import Foundation
+import ToolsboxSDK_Network
+
+enum PokeAPIModule: String {
+	
+	case pokemon
+}
+
+protocol PokeAPIRequest: RequestProtocol {
+	
+	var route: String { get }
+	var module: PokeAPIModule { get }
+}
+
+extension PokeAPIRequest {
+	
+	var scheme: String { "https" }
+	var host: String { "pokeapi.co" }
+	var path: String { "api/v2/\(self.module.rawValue)/\(self.route)/" }
+}
