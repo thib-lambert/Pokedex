e31865c4d59535b9d0dc242ab45847178b67eff6
Thibaud LAMBERT <thib.lambert@icloud.com>
2024-07-14 17:29:33 +0200

    [Project] Initialize

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Thibaud Lambert on 14/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
