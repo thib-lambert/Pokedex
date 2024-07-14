b6dda3c3d9ae596ec453a50ac62d9fa0a200ca9e
Thibaud LAMBERT <thib.lambert@icloud.com>
2024-07-14 18:14:19 +0200

    [Project] Create Pokedex workspace + Create PokedexDataKit

---
+++ PokedexDataKit/PokedexDataKit/PokedexDataKit.h
@@ -0,0 +1,18 @@
+//
+//  PokedexDataKit.h
+//  PokedexDataKit
+//
+//  Created by Thibaud Lambert on 14/07/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PokedexDataKit.
+FOUNDATION_EXPORT double PokedexDataKitVersionNumber;
+
+//! Project version string for PokedexDataKit.
+FOUNDATION_EXPORT const unsigned char PokedexDataKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PokedexDataKit/PublicHeader.h>
+
+
