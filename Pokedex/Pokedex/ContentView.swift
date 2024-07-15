656252c10316a94ee107e7f076ef5ecf779ae7e2
Thibaud LAMBERT <thib.lambert@icloud.com>
2024-07-15 17:42:26 +0200

    [List] Prepare scan button UI

--- Pokedex/Pokedex/ContentView.swift
+++ Pokedex/Pokedex/ContentView.swift
@@ -15,14 +15,31 @@ struct ContentView: View {
 	
 	// MARK: - UI
 	var body: some View {
-		ScrollView {
-			LazyVStack {
-				ForEach(self.pokemons, id: \.id) { pokemon in
-					PokemonCell(pokemon: pokemon)
-						.padding(.bottom, 12)
-						.padding(.horizontal, 16)
+		ZStack(alignment: .bottom) {
+			ScrollView(showsIndicators: false) {
+				LazyVStack {
+					ForEach(self.pokemons, id: \.id) { pokemon in
+						PokemonCell(pokemon: pokemon)
+							.padding(.bottom, 12)
+							.padding(.horizontal, 16)
+					}
 				}
+				.padding(.bottom, 64)
 			}
+			
+			Button(action: {
+				print("OPEN SCAN")
+			}, label: {
+				Image(systemName: "camera.viewfinder")
+					.renderingMode(.template)
+					.resizable()
+					.scaledToFit()
+					.padding(12)
+					.foregroundStyle(.white)
+					.frame(width: 60, height: 60)
+					.background(Color.red)
+					.clipShape(Circle())
+			})
 		}
 		.task {
 			var result: [Pokemon] = []
